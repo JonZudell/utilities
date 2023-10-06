9f868755841c19796938d3da295856d0bc9b452b
Jon Zudell <jonathon.zudell@gmail.com>
2023-10-06 02:24:04 +0000

    ci.sh

---
+++ ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export BOOTSTRAP_PATH=$(realpath "$0")
+export WORKING_DIRECTORY=$(dirname $BOOTSTRAP_PATH)
+
+export REF=$(git rev-parse HEAD)
+echo ${REF:0:8}
+
+docker build . -t registry.digitalocean.com/fugue-state-registry/workflow-task-image:${REF:0:8}
+docker push registry.digitalocean.com/fugue-state-registry/workflow-task-image:${REF:0:8}
