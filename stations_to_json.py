90221decc8fdc239fee8175f86af2da17307d40e
Jon Zudell <jonathon.zudell@gmail.com>
2023-12-01 23:16:53 +0000

    only wmo stations

--- stations_to_json.py
+++ stations_to_json.py
@@ -6,14 +6,20 @@ def write_json(path, stations_list):
     with open(path, "w") as outfile:
         outfile.write(json.dumps(stations_list))
 
-def parse_line(line):
+def get_id(line):
     return line[0:11]
+
+def get_wmo_id(line):
+    return line[80:85].strip()
 
 def read_lines(stations_path):
     results = []
     with open(stations_path) as stations:
         for line in stations.readlines():
-            results.append({"station" : parse_line(line) })
+            station = get_id(line)
+            wmo_id = get_wmo_id(line)
+            if wmo_id:
+                results.append({"station" : get_id(line), "wmo_id" : wmo_id})
     return results
 
 if __name__ == "__main__":
