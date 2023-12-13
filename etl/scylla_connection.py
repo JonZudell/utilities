2bc3f48742b132d0e53d5ec4f21e646c3fc4f915
Jon Zudell <jonathon.zudell@gmail.com>
2023-12-13 00:30:37 +0000

    etl/scylla_connection

--- etl/scylla_connection.py
+++ etl/scylla_connection.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 from cassandra import ConsistencyLevel
-from cassandra.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
-from cassandra.policies import WhiteListRoundRobinPolicy, DowngradingConsistencyRetryPolicy
-from cassandra.query import tuple_factory
+from cassandra.cluster import Cluster
 if __name__ == "__main__":
 
 
-    cluster = Cluster(contact_points=['scylla-us-east-1-us-east-1a-0.scylla.svc.cluster.local'])
+    cluster = Cluster(contact_points=['scylla-us-east-1-us-east-1a-0.scylla.default.svc.cluster.local'])
     session = cluster.connect()
+    session.default_consistency_level = ConsistencyLevel.QUORUM
     session.execute("""
-        CREATE KEYSPACE IF NOT EXISTS keyspace1
-        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}
+        CREATE KEYSPACE IF NOT EXISTS weather
+        WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '2'}
     """)
 
-    session.execute("USE keyspace1")
+    session.execute("USE weather")
     session.execute("""
         CREATE TABLE tbl (pk int PRIMARY KEY, v int)
         WITH per_partition_rate_limit = {'max_writes_per_second': 1}
