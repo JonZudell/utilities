664797e5062978bfc702716ee481a96c1b12dd57
Jon Zudell <jonathon.zudell@gmail.com>
2023-12-15 19:17:12 +0000

    get more data

--- etl/stations_to_json.py
+++ etl/stations_to_json.py
@@ -9,8 +9,20 @@ def write_json(path, stations_list):
 def get_id(line):
     return line[0:11]
 
+def get_latitude(line):
+    return float(line[12:20])
+
+def get_longitude(line):
+    return float(line[21:30])
+
+def get_elevation(line):
+    return float(line[31:37])
+
 def get_wmo_id(line):
     return line[80:85].strip()
+
+def get_name(line):
+    return line[42:72].strip()
 
 def read_lines(stations_path):
     results = []
@@ -19,7 +31,7 @@ def read_lines(stations_path):
             station = get_id(line)
             wmo_id = get_wmo_id(line)
             if wmo_id:
-                results.append({"station" : get_id(line), "wmo_id" : wmo_id})
+                results.append({"station" :station, "wmo_id" : wmo_id, "name" : get_name(line), "latitude" : get_latitude(line), "longitude" : get_longitude(line), "elevation" : get_longitude(line)})
     return results
 
 if __name__ == "__main__":
